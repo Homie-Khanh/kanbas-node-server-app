5fa544e91f0456d53c1d20958eb68f910de3940b
Homie-Khanh <156377690+Homie-Khanh@users.noreply.github.com>
2024-04-24 10:56:45 -0400

    production

--- App.js
+++ App.js
@@ -23,7 +23,8 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: false,
 };
-if (process.env.NODE_ENV !== "development") {
+// if (process.env.NODE_ENV !== "development") {
+if (process.env.NODE_ENV !== "production") {
     sessionOptions.proxy = true;
     sessionOptions.cookie = {
         sameSite: "none",
